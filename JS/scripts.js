864d01bef2f18e3b145274ef5043c776086113aa
Jiwon Kang and Minh Phuong <minh@>
2017-01-24 09:41:11 -0800

    Add Business Logic in JS file add Contact Constructor and Prototype add Address Constructor and Prototype

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,25 @@
 //BUSINESS LOGIC
+//Contact constructor and prototype
+function Contact(first, last){
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
+}
 
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+//Adress constructor and prototype
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function () {
+  return this.street + ", " + this.city + ", " + this.state;
+};
 
 //USER INTERFACE
 $(document).ready(function(){
