7f848cf6cae3d639ad18a4c0384143227611b353
Jiwon Kang and Minh Phuong <jiwon@>
2017-01-24 08:50:11 -0800

    Add Click Listener for Another Address in JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,7 +3,11 @@
 
 //USER INTERFACE
 $(document).ready(function(){
+  // Click Listener for Another Address
   $("#add-address").click(function(){
     $("#new-addresses").append('<div class="new-addresses">' + '<div class="form-group">' +'<label for="new-street"></label>'+'<input type="text" class="form-control new-street">'+'
-    </div>')  });
+    </div>' + '<div class="form-group">' +
+    '<label for="new-city">City</label>' +
+    '<input type="text" class="form-control new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-state">State</label>' + '<input type="text" class="form-control new-state">' + '</div>');
+  });
 });
